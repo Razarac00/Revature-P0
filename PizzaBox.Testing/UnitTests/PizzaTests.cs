39310bc9503af922e5b999f5cad0ea00f47bca2c
Razarac00 <orji.john@outlook.com>
2019-08-12 15:56:46 -0500

    Added empty tests to pizzatests

--- PizzaBox.Testing/UnitTests/PizzaTests.cs
+++ PizzaBox.Testing/UnitTests/PizzaTests.cs
@@ -3,12 +3,60 @@ using Xunit;
 
 namespace PizzaBox.Testing.UnitTests
 {
+    /**
+    ### pizza
+    + should be able to have a size
+    + should be able to compute its cost
+    + should be able to have at least 2 default toppings
+    + should be able to have a crust
+    + should be able to limit its toppings to no more than 5
+     */
     public class PizzaTests
     {
         [Fact]
-        public void Test1()
+        public void PizzaHasSize()
         {
 
+        }
+
+        [Fact]
+        public void PizzaHasCost()
+        {
+        //Given
+        
+        //When
+        
+        //Then
+        }
+
+        [Fact]
+        public void PizzaHasMinTwoToppings()
+        {
+        //Given
+        
+        //When
+        
+        //Then
+        }
+
+        [Fact]
+        public void PizzaHasMaxFiveToppings()
+        {
+        //Given
+        
+        //When
+        
+        //Then
+        }
+
+        [Fact]
+        public void PizzaHasCrust()
+        {
+        //Given
+        
+        //When
+        
+        //Then
         }
     }
 }
