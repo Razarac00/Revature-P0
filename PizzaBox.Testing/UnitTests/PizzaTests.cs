c3579d03d6519cdf4462d876b3768010580b229f
Razarac00 <orji.john@outlook.com>
2019-08-12 15:45:00 -0500

    Skeleton of the Pizza Time

---
+++ PizzaBox.Testing/UnitTests/PizzaTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PizzaBox.Testing.UnitTests
+{
+    public class PizzaTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
