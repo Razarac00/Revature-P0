5e9a852a7edd9259502dfcc54a2a259511a4f93b
Razarac00 <orji.john@outlook.com>
2019-08-13 15:56:25 -0500

    Added empty test cases to ordertests

--- PizzaBox.Testing/UnitTests/OrderTests.cs
+++ PizzaBox.Testing/UnitTests/OrderTests.cs
@@ -1,3 +1,40 @@
 using System;
 using Xunit;
 using PizzaBox.Domain.Models;
+
+namespace PizzaBox.Testing.UnitTests
+{
+    /**
+    ### order
+    + should be able to view its pizzas
+    + should be able to compute its cost
+    + should be able to limit its cost to no more than $5000
+    + should be able to limit its pizza count to no more than 100
+     */
+    public class OrderTests
+    {
+        [Fact]
+        public void CanViewOrder()
+        {
+
+        }
+
+        [Fact]
+        public void CanComputeCost()
+        {
+
+        }
+
+        [Fact]
+        public void CheckCostLimit()
+        {
+
+        }
+
+        [Fact]
+        public void CheckPizzaCount()
+        {
+            
+        }
+    }
+}
