3165fcca06842cc72b82729b6559d42815d635b1
Razarac00 <orji.john@outlook.com>
2019-08-23 09:32:53 -0500

    Placed userlist behaviour back into userlist

--- PizzaBox.Domain/Models/UserList.cs
+++ PizzaBox.Domain/Models/UserList.cs
@@ -15,8 +15,51 @@ namespace PizzaBox.Domain.Models
             if (_users == null)
             {
                 _users = new List<User>();
+                
             }
             return _users;
+        }
+
+        public static User CreateUser(string uName, string fName, string lName, string password)
+        {
+            if (ContainsUserName(uName) == null)
+            {
+                var user = new User();
+                user.UserName = uName;
+                user.Name.First = fName;
+                user.Name.Last = lName;
+                user.Password = password;
+                UserList.Instance().Add(user);
+                return user;
+            }
+            return null;
+        }
+
+        public static User LoginUser(string uName, string password)
+        {
+            var potentialUser = ContainsUserName(uName);
+            if (potentialUser != null && PasswordCheck(potentialUser, password))
+            {
+                return potentialUser;
+            }
+            return null;
+        }
+
+        private static User ContainsUserName(string uName)
+        {
+            foreach (User u in UserList.Instance())
+            {
+                if (u.UserName == uName)
+                {
+                    return u;
+                }
+            }
+            return null;
+        }
+
+        private static bool PasswordCheck(User u, string password)
+        {
+            return u.Password == password;
         }
 
         private UserList() {}
--- PizzaBox.Domain/UIScreens/LoginSignUpScreen.cs
+++ PizzaBox.Domain/UIScreens/LoginSignUpScreen.cs
@@ -33,7 +33,7 @@ namespace PizzaBox.Domain.UIScreens
             Console.WriteLine(_accountPassword);
             string potentialPass = Console.ReadLine();
 
-            return LoginUser(potentialName, potentialPass);    
+            return UserList.LoginUser(potentialName, potentialPass);    
         }
 
         private User CreateUserScreen()
@@ -51,49 +51,7 @@ namespace PizzaBox.Domain.UIScreens
             Console.WriteLine(_accountLastName);
             string potentialLastName = CleanString(Console.ReadLine());
 
-            return CreateUser(potentialName, potentialFirstName, potentialLastName, potentialPass);
-        }
-
-        public User CreateUser(string uName, string fName, string lName, string password)
-        {
-            if (ContainsUserName(uName) == null)
-            {
-                var user = new User();
-                user.UserName = uName;
-                user.Name.First = fName;
-                user.Name.Last = lName;
-                user.Password = password;
-                UserList.Instance().Add(user);
-                return user;
-            }
-            return null;
-        }
-
-        public User LoginUser(string uName, string password)
-        {
-            var potentialUser = ContainsUserName(uName);
-            if (potentialUser != null && PasswordCheck(potentialUser, password))
-            {
-                return potentialUser;
-            }
-            return null;
-        }
-
-        private User ContainsUserName(string uName)
-        {
-            foreach (User u in UserList.Instance())
-            {
-                if (u.UserName == uName)
-                {
-                    return u;
-                }
-            }
-            return null;
-        }
-
-        private bool PasswordCheck(User u, string password)
-        {
-            return u.Password == password;
+            return UserList.CreateUser(potentialName, potentialFirstName, potentialLastName, potentialPass);
         }
 
         public void Begin()
