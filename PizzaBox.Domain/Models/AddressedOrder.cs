158c87f8ba5d996be29553cc6137830f90649285
Razarac00 <orji.john@outlook.com>
2019-08-26 20:08:23 -0500

    Further implementation of user ordering

--- PizzaBox.Domain/Models/AddressedOrder.cs
+++ PizzaBox.Domain/Models/AddressedOrder.cs
@@ -1,13 +1,31 @@
+using System;
+
 namespace PizzaBox.Domain.Models
 {
     public class AddressedOrder
     {
-        public Address Address { get; set; }
-        public Order Order { get; set; }
+        private string _dateFormat = "MM/dd/yyyy HH:mm:ss";
+        private DateTime _date = new DateTime();
+        private Address _address = new Address();
+        private Order _order = new Order();
+        public Address Address { get => _address; set => _address = value; }
+        public Order Order { get => _order; set => _order = value; }
+        public DateTime Date { get => _date; set => _date = value; }
 
         public override string ToString()
         {
-            return $"ADDRESS: {Address.ToString()}\nORDER:\t{Order.ToString()}";
+            return $"DATE: {Date.ToString(_dateFormat)}\nADDRESS: {Address.ToString()}\nORDER:\t{Order.ToString()}";
+        }
+
+        public AddressedOrder(Address address, Order order)
+        {
+            Address = address;
+            Order = order;
+        }
+
+        public AddressedOrder()
+        {
+
         }
     }
 }
--- PizzaBox.Domain/Models/User.cs
+++ PizzaBox.Domain/Models/User.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using PizzaBox.Data.Entities;
 using PizzaBox.Domain.Interfaces;
 
 namespace PizzaBox.Domain.Models
@@ -7,28 +8,46 @@ namespace PizzaBox.Domain.Models
     public class User 
     {
         private Name _name = new Name();
+        private AddressedOrder _latestOrder = new AddressedOrder();
         public Name Name { get => _name; set => _name = value; }
         public string UserName { get; set; }
         public string Password { get; set; }
         public List<AddressedOrder> UserOrderHistory { get; set; }
-        public Address LatestStore { get; set; }
+        public AddressedOrder LatestOrder { get => _latestOrder; set => _latestOrder = value; }
         private AddressedOrder CurrentOrder { get; set; }
         public Pizza CustomPizza { get; set; }
 
         public AddressedOrder StartOrder(Address address)
         {
             var order = new Order();
-            CurrentOrder = new AddressedOrder();
-            CurrentOrder.Address = address;
-            CurrentOrder.Order = order;
+            CurrentOrder = new AddressedOrder(address, order);
             return CurrentOrder;
         }
 
         public AddressedOrder FinishOrder()
         {
-            LatestStore = CurrentOrder.Address;
+            CurrentOrder.Date = DateTime.Now;
 
-            return CurrentOrder;
+            if (LatestOrder is null ||  (CurrentOrder.Date.Hour - LatestOrder.Date.Hour) >= 2)
+            {
+                Save(CurrentOrder);
+                return CurrentOrder;
+            }
+
+            return null;
+        }
+
+        private void Save(AddressedOrder currentOrder)
+        {
+            var db = new projectzeroDBContext();
+            db.Order.Add(new Data.Entities.Order
+            {
+                OrderDate = currentOrder.Date,
+                Active = true
+            });
+            LatestOrder = CurrentOrder;
+            UserOrderHistory.Add(CurrentOrder);
+            //db.SaveChanges();
         }
 
         public void PrintOrderHistory()
