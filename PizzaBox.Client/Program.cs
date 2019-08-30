57ed44b3479448b208c273553be716226e6ab250
Razarac00 <orji.john@outlook.com>
2019-08-29 22:14:51 -0500

    Added MVC project skeleton and removed console project

--- PizzaBox.Client/Program.cs
+++ PizzaBox.Client/Program.cs
@@ -1,53 +1,24 @@
 ﻿using System;
-using PizzaBox.Domain.Models;
-using PizzaBox.Domain.Interfaces;
-using PizzaBox.Domain.Recipes;
 using System.Collections.Generic;
-using PizzaBox.Domain.UIScreens;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace PizzaBox.Client
 {
-    class Program
+    public class Program
     {
-
-
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            var p = new Program();
-            p.Start();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        /**
-        + as a user i should be able to register
-        + as a user i should be able to signin
-        + as a user i should be able to view a list of locations
-        + as a user i should be able to select a location
-        + as a user i should be able to make an order
-        + as a user i should be able to choose custom or preset pizza(s)
-        + as a user i should be able to select a crust
-        + as a user i should be able to select a size
-        + as a user i should be able to select a set of toppings
-        + as a user i should be able to preview my order
-        + as a user i should be able to confirm my order
-        + as a user i should be able to view my order history
-        + as a user i should be able to signout
-        */
-        public void Start()
-        {
-            IntroScreen intro = new IntroScreen();
-            intro.Begin();
-        }
-
-        public void MessWithToppings()
-        {
-            Topping hab = new Topping("habanero");
-            System.Console.WriteLine(hab.Price + " " + hab.Name);
-        }
-
-        public void MakeNewYork()
-        {
-            var ny = new NewYork();
-            ny.Make();
-        } 
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
 }
