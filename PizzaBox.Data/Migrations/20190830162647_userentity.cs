c5eb2ba911eab84235591083a129c8ea947c107c
Razarac00 <orji.john@outlook.com>
2019-08-30 13:09:07 -0500

    Push migrations

---
+++ PizzaBox.Data/Migrations/20190830162647_userentity.cs
@@ -0,0 +1,419 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PizzaBox.Data.Migrations
+{
+    public partial class userentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    AddressId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    StoreId = table.Column<int>(nullable: false),
+                    AddressLine = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.AddressId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stores",
+                columns: table => new
+                {
+                    StoreId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AddressId = table.Column<int>(nullable: false),
+                    LocationAddressId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stores", x => x.StoreId);
+                    table.ForeignKey(
+                        name: "FK_Stores_Addresses_LocationAddressId",
+                        column: x => x.LocationAddressId,
+                        principalTable: "Addresses",
+                        principalColumn: "AddressId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Inventories",
+                columns: table => new
+                {
+                    InventoryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    StoreId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inventories", x => x.InventoryId);
+                    table.ForeignKey(
+                        name: "FK_Inventories_Stores_StoreId",
+                        column: x => x.StoreId,
+                        principalTable: "Stores",
+                        principalColumn: "StoreId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AddressedOrderId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AddressedOrders",
+                columns: table => new
+                {
+                    AddressedOrderId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<int>(nullable: false),
+                    AddressId = table.Column<int>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    FinalPrice = table.Column<decimal>(nullable: false),
+                    OrderHistoryId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AddressedOrders", x => x.AddressedOrderId);
+                    table.ForeignKey(
+                        name: "FK_AddressedOrders_Addresses_AddressId",
+                        column: x => x.AddressId,
+                        principalTable: "Addresses",
+                        principalColumn: "AddressId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InventoryItem",
+                columns: table => new
+                {
+                    InventoryItemId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ItemAItemId = table.Column<int>(nullable: true),
+                    Amount = table.Column<int>(nullable: false),
+                    InventoryId = table.Column<int>(nullable: true),
+                    StoreId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InventoryItem", x => x.InventoryItemId);
+                    table.ForeignKey(
+                        name: "FK_InventoryItem_Inventories_InventoryId",
+                        column: x => x.InventoryId,
+                        principalTable: "Inventories",
+                        principalColumn: "InventoryId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_InventoryItem_Stores_StoreId",
+                        column: x => x.StoreId,
+                        principalTable: "Stores",
+                        principalColumn: "StoreId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pizzas",
+                columns: table => new
+                {
+                    PizzaId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CrustAItemId = table.Column<int>(nullable: true),
+                    SizeAItemId = table.Column<int>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pizzas", x => x.PizzaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AItem",
+                columns: table => new
+                {
+                    AItemId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Price = table.Column<decimal>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Discriminator = table.Column<string>(nullable: false),
+                    CrustId = table.Column<int>(nullable: true),
+                    SizeId = table.Column<int>(nullable: true),
+                    ToppingId = table.Column<int>(nullable: true),
+                    PizzaId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AItem", x => x.AItemId);
+                    table.ForeignKey(
+                        name: "FK_AItem_Pizzas_PizzaId",
+                        column: x => x.PizzaId,
+                        principalTable: "Pizzas",
+                        principalColumn: "PizzaId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserName = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    CustomPizzaPizzaId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                    table.ForeignKey(
+                        name: "FK_Users_Pizzas_CustomPizzaPizzaId",
+                        column: x => x.CustomPizzaPizzaId,
+                        principalTable: "Pizzas",
+                        principalColumn: "PizzaId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Name",
+                columns: table => new
+                {
+                    NameId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<int>(nullable: false),
+                    First = table.Column<string>(nullable: true),
+                    Last = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Name", x => x.NameId);
+                    table.ForeignKey(
+                        name: "FK_Name_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderHistories",
+                columns: table => new
+                {
+                    OrderHistoryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<int>(nullable: false),
+                    StoreId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderHistories", x => x.OrderHistoryId);
+                    table.ForeignKey(
+                        name: "FK_OrderHistories_Stores_StoreId",
+                        column: x => x.StoreId,
+                        principalTable: "Stores",
+                        principalColumn: "StoreId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderHistories_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AddressedOrders_AddressId",
+                table: "AddressedOrders",
+                column: "AddressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AddressedOrders_OrderHistoryId",
+                table: "AddressedOrders",
+                column: "OrderHistoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AddressedOrders_UserId",
+                table: "AddressedOrders",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AItem_PizzaId",
+                table: "AItem",
+                column: "PizzaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inventories_StoreId",
+                table: "Inventories",
+                column: "StoreId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InventoryItem_InventoryId",
+                table: "InventoryItem",
+                column: "InventoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InventoryItem_ItemAItemId",
+                table: "InventoryItem",
+                column: "ItemAItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InventoryItem_StoreId",
+                table: "InventoryItem",
+                column: "StoreId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Name_UserId",
+                table: "Name",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderHistories_StoreId",
+                table: "OrderHistories",
+                column: "StoreId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderHistories_UserId",
+                table: "OrderHistories",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_AddressedOrderId",
+                table: "Orders",
+                column: "AddressedOrderId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pizzas_CrustAItemId",
+                table: "Pizzas",
+                column: "CrustAItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pizzas_SizeAItemId",
+                table: "Pizzas",
+                column: "SizeAItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stores_LocationAddressId",
+                table: "Stores",
+                column: "LocationAddressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_CustomPizzaPizzaId",
+                table: "Users",
+                column: "CustomPizzaPizzaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserName",
+                table: "Users",
+                column: "UserName",
+                unique: true,
+                filter: "[UserName] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_AddressedOrders_AddressedOrderId",
+                table: "Orders",
+                column: "AddressedOrderId",
+                principalTable: "AddressedOrders",
+                principalColumn: "AddressedOrderId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AddressedOrders_OrderHistories_OrderHistoryId",
+                table: "AddressedOrders",
+                column: "OrderHistoryId",
+                principalTable: "OrderHistories",
+                principalColumn: "OrderHistoryId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AddressedOrders_Users_UserId",
+                table: "AddressedOrders",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_InventoryItem_AItem_ItemAItemId",
+                table: "InventoryItem",
+                column: "ItemAItemId",
+                principalTable: "AItem",
+                principalColumn: "AItemId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pizzas_AItem_CrustAItemId",
+                table: "Pizzas",
+                column: "CrustAItemId",
+                principalTable: "AItem",
+                principalColumn: "AItemId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pizzas_AItem_SizeAItemId",
+                table: "Pizzas",
+                column: "SizeAItemId",
+                principalTable: "AItem",
+                principalColumn: "AItemId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AItem_Pizzas_PizzaId",
+                table: "AItem");
+
+            migrationBuilder.DropTable(
+                name: "InventoryItem");
+
+            migrationBuilder.DropTable(
+                name: "Name");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Inventories");
+
+            migrationBuilder.DropTable(
+                name: "AddressedOrders");
+
+            migrationBuilder.DropTable(
+                name: "OrderHistories");
+
+            migrationBuilder.DropTable(
+                name: "Stores");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Addresses");
+
+            migrationBuilder.DropTable(
+                name: "Pizzas");
+
+            migrationBuilder.DropTable(
+                name: "AItem");
+        }
+    }
+}
