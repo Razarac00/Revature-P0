80aeba1ec79b0995980c0f7b3b72057dc4830b82
Razarac00 <orji.john@outlook.com>
2019-08-30 10:13:15 -0500

    Added dbsets to context, made 1to1 between user and name

--- PizzaBox.Data/ProjectZeroTwoDBContext.cs
+++ PizzaBox.Data/ProjectZeroTwoDBContext.cs
@@ -5,6 +5,16 @@ namespace PizzaBox.Data
 {
     public class ProjectZeroTwoDBContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<AddressedOrder> AddressedOrders { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Pizza> Pizzas { get; set; }
+        public DbSet<Topping> Toppings { get; set; }
+        public DbSet<Crust> Crusts { get; set; }
+        public DbSet<Size> Sizes { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
             builder.UseSqlServer("Server=tcp:project-zero-john.database.windows.net,1433;Initial Catalog=ProjectZeroTwoDB;Persist Security Info=False;User ID=sqladmin;Password=Mafia123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
--- PizzaBox.Domain/Models/Name.cs
+++ PizzaBox.Domain/Models/Name.cs
@@ -3,6 +3,8 @@ namespace PizzaBox.Domain.Models
     public class Name
     {
         public int NameId { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
         public string First { get; set; }
         public string Last { get; set; }
     }
