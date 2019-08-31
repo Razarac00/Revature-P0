1708d3974213c4d5f2d60864a646fc44fd57306b
Razarac00 <orji.john@outlook.com>
2019-08-31 16:59:21 -0500

    Fleshed out some cshtml, tried to add a button

--- PizzaBox.Client/Controllers/UserController.cs
+++ PizzaBox.Client/Controllers/UserController.cs
@@ -14,6 +14,13 @@ namespace PizzaBox.Client.Controllers
     {
         private ProjectZeroTwoDBContext _db = new ProjectZeroTwoDBContext();
 
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         [HttpPost]
         public IActionResult Login(string username, string password)
         {
@@ -28,6 +35,12 @@ namespace PizzaBox.Client.Controllers
                 return RedirectToAction("Error");
             }
 
+        }
+
+        [HttpGet]
+        public IActionResult CreateUser()
+        {
+            return View();
         }
 
         [HttpPost]
