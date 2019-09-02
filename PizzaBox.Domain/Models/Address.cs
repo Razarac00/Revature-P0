dc1e800e9d3ada384220bdcfce6493b5cdf9de97
Razarac00 <orji.john@outlook.com>
2019-09-02 17:43:49 -0500

    added store creation

--- PizzaBox.Domain/Models/Address.cs
+++ PizzaBox.Domain/Models/Address.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PizzaBox.Domain.Models
 {
     public class Address
     {
         public int AddressId { get; set; }
         public int StoreId { get; set; }
+
+        [Required(ErrorMessage="Address Required")]
         public string AddressLine { get; set; }
+
+        [Required(ErrorMessage="City Required")]
         public string City { get; set; }
 
         public Address()
