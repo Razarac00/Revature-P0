7d886e5e2e034aaf2ad03f53fd2a6e835a2eab8d
Razarac00 <orji.john@outlook.com>
2019-09-03 01:18:20 -0500

    Structured order form a bit

--- PizzaBox.Client/Controllers/OrderController.cs
+++ PizzaBox.Client/Controllers/OrderController.cs
@@ -18,15 +18,24 @@ namespace PizzaBox.Client.Controllers
         [HttpGet]
         public IActionResult CreateOrder()
         {
+            var storeAddresses = _db.Addresses.ToList();
+            var toppings = _db.Toppings.ToList();
+            var crusts = _db.Crusts.ToList();
+            var sizes = _db.Sizes.ToList();
+            ViewBag.StoreAddresses = storeAddresses;
+            ViewBag.Toppings = toppings;
+            ViewBag.Crusts = crusts;
+            ViewBag.Sizes = sizes;
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult CreateOrder(int userId, int addressId)
+        public IActionResult CreateOrder(AddressedOrder addressedOrder, int addressId)
         {
             if (ModelState.IsValid)
             {
+                var userId = (int) TempData["UserID"];
                 var currentAddress = _db.Addresses.Single(a => a.AddressId == addressId);
                 var currentUser = _db.Users.Single(u => u.UserId == userId);
                 AddressedOrder finalOrder = new AddressedOrder();
